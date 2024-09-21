ef959475713afb2a14c72ba6cd636f136f449c5b
Werner Strydom <bloudraak@users.noreply.github.com>
2024-09-21 07:11:27 -0700

    Evolving the Configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,65 @@
+
 resource "github_organization_settings" "this" {
-  billing_email = var.billing_email
+  billing_email                                                = var.billing_email
+  company                                                      = var.company
+  blog                                                         = var.blog
+  email                                                        = var.email
+  twitter_username                                             = var.twitter_username
+  location                                                     = var.location
+  name                                                         = var.name
+  description                                                  = var.description
+  has_organization_projects                                    = var.has_organization_projects
+  has_repository_projects                                      = var.has_repository_projects
+  default_repository_permission                                = var.default_repository_permission
+  members_can_create_repositories                              = var.members_can_create_repositories
+  members_can_create_public_repositories                       = var.members_can_create_public_repositories
+  members_can_create_private_repositories                      = var.members_can_create_private_repositories
+  members_can_create_internal_repositories                     = var.members_can_create_internal_repositories
+  members_can_create_pages                                     = var.members_can_create_pages
+  members_can_create_public_pages                              = var.members_can_create_public_pages
+  members_can_create_private_pages                             = var.members_can_create_private_pages
+  members_can_fork_private_repositories                        = var.members_can_fork_private_repositories
+  web_commit_signoff_required                                  = var.web_commit_signoff_required
+  advanced_security_enabled_for_new_repositories               = var.advanced_security_enabled_for_new_repositories
+  dependabot_alerts_enabled_for_new_repositories               = var.dependabot_alerts_enabled_for_new_repositories
+  dependabot_security_updates_enabled_for_new_repositories     = var.dependabot_security_updates_enabled_for_new_repositories
+  dependency_graph_enabled_for_new_repositories                = var.dependency_graph_enabled_for_new_repositories
+  secret_scanning_enabled_for_new_repositories                 = var.secret_scanning_enabled_for_new_repositories
+  secret_scanning_push_protection_enabled_for_new_repositories = var.secret_scanning_push_protection_enabled_for_new_repositories
+}
+
+resource "github_repository" "this" {
+  name                   = ".github"
+  allow_auto_merge       = true
+  allow_merge_commit     = false
+  allow_rebase_merge     = true
+  allow_squash_merge     = true
+  archived               = false
+  allow_update_branch    = true
+  visibility             = "public"
+  has_issues             = false
+  has_projects           = false
+  has_wiki               = false
+  is_template            = false
+  archive_on_destroy     = true
+  auto_init              = true
+  delete_branch_on_merge = true
+  has_discussions        = false
+  homepage_url           = var.blog
+  vulnerability_alerts   = true
+}
+
+resource "github_repository_file" "this" {
+  repository          = github_repository.this.name
+  file                = "profile/README.md"
+  commit_author       = var.commit_author
+  commit_email        = var.commit_email
+  commit_message      = var.commit_message
+  branch              = "main"
+  overwrite_on_create = false
+  content             = <<EOF
+# ${var.name == "" ? title(var.owner) : title(var.name)}
+
+${var.description}
+EOF
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,169 @@ variable "owner" {
   type        = string
   description = "The owner of the organization."
 }
+
+variable "company" {
+  type        = string
+  description = "The company name for the organization."
+  default     = null
+}
+
+variable "blog" {
+  type        = string
+  description = "The blog URL for the organization."
+  default     = null
+}
+
+variable "email" {
+  type        = string
+  description = "The email for the organization."
+  default     = ""
+}
+
+variable "twitter_username" {
+  type        = string
+  description = "The Twitter username for the organization."
+  default     = ""
+}
+
+variable "location" {
+  type        = string
+  description = "The location for the organization."
+  default     = ""
+}
+
+variable "name" {
+  type        = string
+  description = "The name for the organization."
+  default     = ""
+}
+
+variable "description" {
+  type        = string
+  description = "The description for the organization."
+  default     = ""
+}
+
+variable "has_organization_projects" {
+  type        = bool
+  description = "Whether organization projects are enabled."
+  default     = true
+}
+
+variable "has_repository_projects" {
+  type        = bool
+  description = "Whether repository projects are enabled."
+  default     = true
+}
+
+variable "default_repository_permission" {
+  type        = string
+  description = "The default permission for repositories."
+  default     = "read"
+}
+
+variable "members_can_create_repositories" {
+  type        = bool
+  description = "Whether members can create repositories."
+  default     = true
+}
+
+variable "members_can_create_public_repositories" {
+  type        = bool
+  description = "Whether members can create public repositories."
+  default     = true
+}
+
+variable "members_can_create_private_repositories" {
+  type        = bool
+  description = "Whether members can create private repositories."
+  default     = true
+}
+
+variable "members_can_create_internal_repositories" {
+  type        = bool
+  description = "Whether members can create internal repositories."
+  default     = true
+}
+
+variable "members_can_create_pages" {
+  type        = bool
+  description = "Whether members can create pages."
+  default     = true
+}
+
+variable "members_can_create_public_pages" {
+  type        = bool
+  description = "Whether members can create public pages."
+  default     = true
+}
+
+variable "members_can_create_private_pages" {
+  type        = bool
+  description = "Whether members can create private pages."
+  default     = true
+}
+
+variable "members_can_fork_private_repositories" {
+  type        = bool
+  description = "Whether members can fork private repositories."
+  default     = true
+}
+
+variable "web_commit_signoff_required" {
+  type        = bool
+  description = "Whether web commit signoff is required."
+  default     = false
+}
+
+variable "advanced_security_enabled_for_new_repositories" {
+  type        = bool
+  description = "Whether advanced security is enabled for new repositories."
+  default     = false
+}
+
+variable "dependabot_alerts_enabled_for_new_repositories" {
+  type        = bool
+  description = "Whether Dependabot alerts are enabled for new repositories."
+  default     = false
+}
+
+variable "dependabot_security_updates_enabled_for_new_repositories" {
+  type        = bool
+  description = "Whether Dependabot security updates are enabled for new repositories."
+  default     = false
+}
+
+variable "dependency_graph_enabled_for_new_repositories" {
+  type        = bool
+  description = "Whether dependency graph is enabled for new repositories."
+  default     = false
+}
+
+variable "secret_scanning_enabled_for_new_repositories" {
+  type        = bool
+  description = "Whether secret scanning is enabled for new repositories."
+  default     = false
+}
+
+variable "secret_scanning_push_protection_enabled_for_new_repositories" {
+  type        = bool
+  description = "Whether secret scanning push protection is enabled for new repositories."
+  default     = false
+}
+
+variable "commit_author" {
+  type        = string
+  description = "The author of the commit."
+}
+
+variable "commit_email" {
+  type        = string
+  description = "The email of the commit author."
+}
+
+variable "commit_message" {
+  type        = string
+  description = "The message of the initial commit."
+  default     = "Initial commit"
+}
